583aac204eeb841f13e03ff739a26d8e7c6626d7
bea <bianca.subion@gmail.com>
2026-02-14 17:59:19 -0800

    replace meepachat casks with meepagateway cask

---
+++ Casks/meepagateway.rb
@@ -0,0 +1,14 @@
+cask "meepagateway" do
+  version "0.1.0"
+
+  on_arm do
+    sha256 "67e6b6215917b31e1a00f483403e623ff7e56a92985a086f2278eb679c3861ea"
+    url "https://github.com/bogpad/homebrew-tap/releases/download/v#{version}/meepagateway_#{version}_darwin_arm64.tar.gz"
+  end
+
+  name "MeepaGateway"
+  desc "Bot gateway for conversational AI agents"
+  homepage "https://github.com/bogpad/meepa"
+
+  binary "meepagateway"
+end
