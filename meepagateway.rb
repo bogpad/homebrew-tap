838d5c81bdb4fa0cfa3bf56e91d8bc97b0095fb3
Bianca <bianca.subion@gmail.com>
2026-02-16 03:00:02 +0000

    Update meepagateway formula to 0.2.0

--- meepagateway.rb
+++ meepagateway.rb
@@ -9,22 +9,22 @@ class Meepagateway < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/bogpad/homebrew-tap/releases/download/gateway-v0.2.0/meepagateway-0.2.0-macos-amd64.tar.gz"
-      sha256 "64f9e65b5926660e80b0321b5bd6aefc1c8f7c9b16c89b5ff7404a1646da3c5b"
+      sha256 "585d626922b545458f3e201519570b3cf05194b216aef380c8420d92bd9ad811"
     end
     if Hardware::CPU.arm?
       url "https://github.com/bogpad/homebrew-tap/releases/download/gateway-v0.2.0/meepagateway-0.2.0-macos-arm64.tar.gz"
-      sha256 "a4ed27bde0dbec54fc8e54592a9e3517fc2c2bd64c8a352053faf04021a5044f"
+      sha256 "22fa086a0b726e7849280c9065b7ec4bda05acccf2f5a94158cb4b3964286b41"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
       url "https://github.com/bogpad/homebrew-tap/releases/download/gateway-v0.2.0/meepagateway-0.2.0-linux-amd64.tar.gz"
-      sha256 "8f2604999db2ec93277bace84ee9285112daecb23f6198f107a30b7fb7d1fa9f"
+      sha256 "b8e3a75695f3b960662b6bc4303ec25df09bd8fb142ab5bd4354e6b5f761c397"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/bogpad/homebrew-tap/releases/download/gateway-v0.2.0/meepagateway-0.2.0-linux-arm64.tar.gz"
-      sha256 "ded1224e6cbdbc204765b662d533a02e3bc9167e5b632d952e6a36dd248f34a8"
+      sha256 "86fce88884e754044d0cadfdda5ccd834b0294f36bc94e47506251141a494e2b"
     end
   end
 
