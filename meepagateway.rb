3b27ce3a8b8d94f619bc70e7c3fdcbec612e7b4f
Bianca <bianca.subion@gmail.com>
2026-02-23 23:07:14 +0000

    Update meepagateway formula to 0.4.44

--- meepagateway.rb
+++ meepagateway.rb
@@ -4,27 +4,27 @@
 class Meepagateway < Formula
   desc "MeepaGateway - Bot gateway for conversational AI agents"
   homepage "https://github.com/bogpad/meepa"
-  version "0.4.43"
+  version "0.4.44"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.43/meepagateway-0.4.43-darwin-amd64.tar.gz"
-      sha256 "835cb65d65284d23e780d504959baac76405ca53693755d5fb1672931481210a"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.44/meepagateway-0.4.44-darwin-amd64.tar.gz"
+      sha256 "5a07e9b2e0ef76adddecba5973adf422cb82fe66770827ab98f00f5eadec588d"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.43/meepagateway-0.4.43-darwin-arm64.tar.gz"
-      sha256 "d11b1be780c3d59b8a9312100fbdb98894f9113e0c18d45086822ef355d694df"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.44/meepagateway-0.4.44-darwin-arm64.tar.gz"
+      sha256 "6c52ebe7672eaef2de802534ebfc82866545656f25bc47e986f5fb4d4e350e3b"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.43/meepagateway-0.4.43-linux-amd64.tar.gz"
-      sha256 "6ad8a4d21b0ec17a45a9ad17801fa5d3c14d29dd63adf986a62be9ef3b3aaa11"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.44/meepagateway-0.4.44-linux-amd64.tar.gz"
+      sha256 "81c558416bafe351574abe8535973b6af25ca15699c07329e8a6723fb70673c3"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.43/meepagateway-0.4.43-linux-arm64.tar.gz"
-      sha256 "71f7edfb3a4eccf416a7d609c440c4a0cdeec09ccc3592c12adef95a0b824f26"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.4.44/meepagateway-0.4.44-linux-arm64.tar.gz"
+      sha256 "41f31469c63fc93e57aca61b307b6b03dad5b4d61d74bd49e8c84985fd9bc240"
     end
   end
 
