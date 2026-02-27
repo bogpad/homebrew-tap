5463360dfcfddb9fa3ef8ca21036554553bb0536
Bianca <bianca.subion@gmail.com>
2026-02-27 08:26:10 +0000

    Update meepagateway formula to 0.5.8

--- meepagateway.rb
+++ meepagateway.rb
@@ -4,27 +4,27 @@
 class Meepagateway < Formula
   desc "MeepaGateway - Bot gateway for conversational AI agents"
   homepage "https://github.com/bogpad/meepa"
-  version "0.5.7"
+  version "0.5.8"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.7/meepagateway-0.5.7-darwin-amd64.tar.gz"
-      sha256 "bde69f1b0463c0ab2201bfe3526b1c0bdc526a4a118feb167599aab6a07398ce"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.8/meepagateway-0.5.8-darwin-amd64.tar.gz"
+      sha256 "351b4caefaf341e187b16dc2d7f85ae41d8fb5d7f766f99df53f6e12ba5a3215"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.7/meepagateway-0.5.7-darwin-arm64.tar.gz"
-      sha256 "921c7cd0121ca81cbe1e8c797baa00e829cfaae1965e23a8fde4138834196adb"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.8/meepagateway-0.5.8-darwin-arm64.tar.gz"
+      sha256 "8d4a74725031a6c4894eb9103b95bf505cf0fd75f3f54cfed59b0a314fe51a2d"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.7/meepagateway-0.5.7-linux-amd64.tar.gz"
-      sha256 "d08e777d1e1398486e7ee6302dc92c1f34c78e48989bf30e241925cbef78cbc1"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.8/meepagateway-0.5.8-linux-amd64.tar.gz"
+      sha256 "cde98bafc30e4a3c4e6c5736ef37c77738ff478feb8e22179de4325378357409"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.7/meepagateway-0.5.7-linux-arm64.tar.gz"
-      sha256 "e23cccd0e91d93fd2b65678093221b3cf88b8af9e5208244e99506611e882431"
+      url "https://github.com/bogpad/meepagateway/releases/download/v0.5.8/meepagateway-0.5.8-linux-arm64.tar.gz"
+      sha256 "ed8cb722e306702490e40694fedede9d4595d32bf55457d50858e8d4f9368528"
     end
   end
 
